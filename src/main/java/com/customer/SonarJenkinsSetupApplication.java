5a7ff7a84e022985fcee5f8307f8f958537274cd
ravik07 <kharteravi7@gmail.com>
2025-04-07 19:54:07 +0530

    first commit

---
+++ src/main/java/com/customer/SonarJenkinsSetupApplication.java
@@ -0,0 +1,13 @@
+package com.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarJenkinsSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarJenkinsSetupApplication.class, args);
+	}
+
+}
